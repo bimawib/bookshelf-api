5b5c6a45a87fa21bf8e4f46f08e5ac50c6ed044b
Bima Sahidsyah Putra Wibowo <bimaencun@gmail.com>
2023-04-11 12:53:31 +0700

    adding linter airbnb style

--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
-console.log('hi')
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+    router: {
+      stripTrailingSlash: true,
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`server berjalan pada ${server.info.uri}`);
+};
+
+init();
